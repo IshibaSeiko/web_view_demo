304ab1bbad9fd16130b80505b5ed0d8c4737f519
SeikoHAYASHI <seiko.hayashi@awarefy.com>
2024-06-14 20:10:41 +0900

    chore: Add webview_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late final WebViewController _controller;
+
+  int progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    final WebViewController controller =
+        WebViewController.fromPlatformCreationParams(
+            const PlatformWebViewControllerCreationParams());
+    // #enddocregion platform_features
+
+    controller
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            setState(() {
+              this.progress = progress;
+            });
+            debugPrint('WebView is loading (progress : $progress%)');
+          },
         ),
-      ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+
+    _controller = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter WebView example'),
+          ),
+          body: progress < 100
+              ? const Center(child: CircularProgressIndicator())
+              : WebViewWidget(controller: _controller)),
     );
   }
 }
